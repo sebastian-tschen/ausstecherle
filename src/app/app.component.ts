7c538e9005493582ca3fea5a81eae520965744e9
sebastian-tschen <sebastian.tschen.behrens@gmail.com>
2020-01-27 22:43:27 +0100

    create a canvas to draw on

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,6 +10,6 @@ export class AppComponent implements OnInit {
   name = 'Angular';
 
   ngOnInit(){
-        paper.install( window );
+    
   }
 }
--- src/app/drawing-board/drawing-board.component.ts
+++ src/app/drawing-board/drawing-board.component.ts
@@ -1,16 +1,31 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, ViewChild } from '@angular/core';
+import {paper} from 'paper';
 
 @Component({
   selector: 'app-drawing-board',
   templateUrl: './drawing-board.component.html',
   styleUrls: ['./drawing-board.component.css']
 })
-export class DrawingBoardComponent implements OnInit {
+export class DrawingBoardComponent implements AfterViewInit {
 
-  constructor() { }
+ @ViewChild('canvas',{static:false}) canvas: ElementRef;
 
-  ngOnInit() {
-      paper.setup('paperCanvas');
+  circle: paper.Path;
+  ngAfterViewInit ()
+  {
+    // create a new scope
+    const scope = new paper.PaperScope();
+    // bind it to the canvas
+    scope.setup(this.canvas.nativeElement);
+    // draw
+
+    this.circle = new paper.Path.Ellipse( 
+      new paper.Rectangle( 20,20,50,50 ) );
+    
+    this.circle.strokeColor = "green";
+    this.circle.strokeWidth = 3;
+
   }
 
+  
 }
