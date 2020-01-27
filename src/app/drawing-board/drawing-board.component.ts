6885a21bcf394523ebdd46093e0b8aa9881f4611
sebastian-tschen <sebastian.tschen.behrens@gmail.com>
2020-01-27 23:03:50 +0100

    simplify paths and make handles editable

--- src/app/drawing-board/drawing-board.component.ts
+++ src/app/drawing-board/drawing-board.component.ts
@@ -1,5 +1,5 @@
 import { AfterViewInit, Component, ElementRef, ViewChild } from "@angular/core";
-import { paper } from "paper";
+import { paper, PointText, Point, Path, project } from "paper";
 
 @Component({
   selector: "app-drawing-board",
@@ -11,37 +11,84 @@ export class DrawingBoardComponent implements AfterViewInit {
 
   path: paper.Path;
   circle: paper.Path;
+  
+  handle;
+  scope;
   ngAfterViewInit() {
     // create a new scope
-    const scope = new paper.PaperScope();
+    this.scope = new paper.PaperScope();
     // bind it to the canvas
-    scope.setup(this.canvas.nativeElement);
+    this.scope.setup(this.canvas.nativeElement);
     // draw
 
-    this.circle = new paper.Path.Ellipse(new paper.Rectangle(20,20,100,100))
+    this.circle = new paper.Path.Ellipse(new paper.Rectangle(20, 20, 100, 100));
 
-    this.circle.strokeColor = "blue";
+    this.circle.strokeColor = "green";
     this.circle.strokeWidth = 3;
 
-    scope.view.onMouseDown = this.onMouseDown.bind(this)
-    scope.view.onMouseDrag = this.onMouseDrag.bind(this)
+    this.scope.view.onMouseDown = this.onMouseDown.bind(this);
+    this.scope.view.onMouseDrag = this.onMouseDrag.bind(this);
+    this.scope.view.onMouseUp = this.onMouseUp.bind(this);
   }
-
   onMouseDown(event) {
-    // If we produced a path before, deselect it:
-    if (this.path) {
-      this.path.selected = false;
-    }
 
-    // Create a new path and set its stroke color to black:
-    this.path = new paper.Path({
-      segments: [event.point],
-      strokeColor: "black",
-      // Select the path, so we can see its segment points:
-      fullySelected: true
+    this.handle = null;
+    // Do a hit test on path for handles:
+    var hitResult = this.scope.project.hitTest(event.point, {
+      tolerance: 2,
+      handles: true
     });
+    if (hitResult) {
+      if (hitResult.type == "handle-in") {
+        this.handle = hitResult.segment.handleIn;
+      } else if (hitResult.type == "handle-out") {
+        this.handle = hitResult.segment.handleOut;
+      }
+    }
+    if (this.handle == null) {
+      // If we produced a path before, deselect it:
+      if (this.path) {
+        this.path.selected = false;
+      }
+
+      // Create a new path and set its stroke color to black:
+      this.path = new Path({
+        segments: [event.point],
+        strokeColor: "red",
+        strokeWidth: 10,
+        // Select the path, so we can see its segment points:
+        fullySelected: true
+      });
+    }
   }
+
+  // While the user drags the mouse, points are added to the path
+  // at the position of the mouse:
   onMouseDrag(event) {
-    this.path.add(event.point);
+    if (this.handle) {
+      this.handle.x += event.delta.x;
+      this.handle.y += event.delta.y;
+    } else if (this.path) {
+      this.path.add(event.point);
+
+    }
+  }
+
+  // When the mouse is released, we simplify the path:
+  onMouseUp(event) {
+    if (this.handle == null) {
+      var segmentCount = this.path.segments.length;
+
+      // When the mouse is released, simplify it:
+      this.path.simplify(10);
+
+      // Select the path, so we can see its segments:
+      this.path.fullySelected = true;
+
+      var newSegmentCount = this.path.segments.length;
+      var difference = segmentCount - newSegmentCount;
+      var percentage = 100 - Math.round((newSegmentCount / segmentCount) * 100);
+      
+    }
   }
 }
